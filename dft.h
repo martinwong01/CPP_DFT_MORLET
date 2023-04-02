d26e1269160f5b51cc70652311b4336c8e11d102
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-02 12:37:15 +0800

    Update dft.h

--- dft.h
+++ dft.h
@@ -163,17 +163,14 @@ void fft_func(complex<Type> *data,complex<Type> *out,int N,int Product,Type pi,i
     j = N>>2;
     for(i=1;i<=k;i++) roots[i] = roots[i-1]*c1[0];                   //     1/8 quadrant values
     if(sign > 0) {
-        for(i=1;i<k;i++) roots[j-i] = roots[i].swap().reverse();   
-    } else {
-        for(i=1;i<k;i++) roots[j-i] = roots[i].swap();    
-    }
-    if(sign > 0) {
+        for(i=1;i<k;i++) roots[j-i] = roots[i].swap().reverse();     //     values in remaining quadrant 
         for(i=0;i<j;i++) {
             roots[i+j] = roots[i].turnright();
 	    roots[i+(j<<1)] = roots[i].reverse();
             roots[i+(j<<2)-j] = roots[i].turnleft();
         }
     } else {
+        for(i=1;i<k;i++) roots[j-i] = roots[i].swap();
         for(i=0;i<j;i++) {
             roots[i+j] = roots[i].turnleft();
 	    roots[i+(j<<1)] = roots[i].reverse();

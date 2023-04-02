39bc699e1d44c34e88ffc78a018f66f6be67b136
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-02 21:54:52 +0800

    Update morlet.h

--- morlet.h
+++ morlet.h
@@ -13,8 +13,10 @@ void morlet(Type *data,complex<Type> **transform,int N,int S,Type param,Type dx,
     complex<Type> thread_local dft_product[maxS][maxN];
     complex<Type> thread_local datacomplex[maxN];
 
+    #pragma omp simd
     for(s=0;s<S;s++) scale[s] = pow(2.,s/4.);                                        // set scales with dj=0.25
 
+    #pragma omp simd
     for(k=0;k<N;k++)
         if(k <= N/2)
             freq[k] = 2.*pi*k/N/dx;
@@ -22,6 +24,7 @@ void morlet(Type *data,complex<Type> **transform,int N,int S,Type param,Type dx,
             freq[k] = 2.*pi*(k-N)/N/dx; 
 
     for(s=0;s<S;s++)
+    #pragma omp simd
     for(k=0;k<N;k++)
 	if(freq[k] > 0.) {
             wavefunc[s][k] = exp(-0.5*pow(scale[s]*freq[k]-param,2.))/pow(pi,0.25);
@@ -30,11 +33,13 @@ void morlet(Type *data,complex<Type> **transform,int N,int S,Type param,Type dx,
 	    //wavefunc[s][k] = 0.;
 	}
     
+    #pragma omp simd
     for(n=0;n<N;n++) datacomplex[n].setrealimga(data[n],0);
     
     dft_func<Type>(datacomplex,dft,N,1,pi,1);
 
     for(s=0;s<S;s++)
+    #pragma omp simd
     for(k=0;k<N;k++) {
         dft_product[s][k] = dft[k]*wavefunc[s][k];
     }
@@ -45,6 +50,7 @@ void morlet(Type *data,complex<Type> **transform,int N,int S,Type param,Type dx,
 
     for(s=0;s<S;s++) {
         a = sqrt(2.*pi*scale[s]/dx);
+	#pragma omp simd
         for(n=0;n<N;n++) {
             transform[s][n] *= a;
         }

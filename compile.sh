4abf83325bb1f9aa34e7b6a6bf0ee3d2f6fe3310
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-07 17:54:00 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 
 optim="-fopenmp -Ofast -fopt-info-vec-all"
 
+g++ $optim -o table.exe table.cpp
+table.exe > table.h
+
 g++ $optim -o testdft.exe testdft.cpp
 g++ $optim -o testmorlet.exe testmorlet.cpp
 g++ $optim -o wavelet2d.exe wavelet2d.cpp

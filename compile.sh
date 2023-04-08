25660c649798b04332fa9a6ae29428369f2828d8
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-08 16:18:55 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 optim="-fopenmp -Ofast -fopt-info-vec-all"
 
 g++ $optim -o table.exe table.cpp
-table.exe > table.h
+./table.exe > table.h
 
 g++ $optim -o testdft.exe testdft.cpp
 g++ $optim -o testmorlet.exe testmorlet.cpp

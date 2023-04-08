78f609c4eca20a7659827982868808ff35507f48
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-08 14:50:31 +0800

    Update dft.h

--- dft.h
+++ dft.h
@@ -1,5 +1,5 @@
 #define maxN 131072 
-#define maxCooleyTukey 60                                       // if prime factor larger than this, use Rader algorithm
+#define maxCooleyTukey 40                                       // if prime factor larger than this, use Rader algorithm
 #define fft_bit_reverse 0
 
 #if fft_bit_reverse == 1

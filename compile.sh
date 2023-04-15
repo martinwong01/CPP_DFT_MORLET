d46cc5bd22849c67cce42b1cfa02435d181c79a1
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-04-15 13:07:47 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-optim="-fopenmp -Ofast -fopt-info-vec-all"
+optim="-fopenmp -Ofast"
 
 g++ $optim -o table.exe table.cpp
 ./table.exe > table.h
 
-g++ $optim -o testdft.exe testdft.cpp
-g++ $optim -o testmorlet.exe testmorlet.cpp
-g++ $optim -o wavelet2d.exe wavelet2d.cpp
+#g++ $optim -o testdft.exe testdft.cpp
+#g++ $optim -o testmorlet.exe testmorlet.cpp
+#g++ $optim -o wavelet2d.exe wavelet2d.cpp
+
+
+checkavx=$(grep "avx" /proc/cpuinfo|wc -l)
+#checkavx=0
+g++ -Ofast -mavx -o fft_bench.exe fft_bench.cpp -D"AVX=${checkavx}"
+

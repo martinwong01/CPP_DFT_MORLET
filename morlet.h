0237c936f73d5487e34cb529abe7ee00585484de
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2023-05-07 15:09:00 +0800

    Update morlet.h

--- morlet.h
+++ morlet.h
@@ -45,15 +45,15 @@ void morlet(Type *data,complex<Type> **transform,int N,int S,Type param,Type dx,
     if(init == 1) {
         a = 2.*pi/N/dx;
         b = 1./pow(pi,0.25);
-        for(s=0;s<S;s++) scale[s] = pow(2.,s/4.);                                        // set scales with dj=0.25
-	for(k=0;k<=N/2;k++) freq[k] = a*k;                                               // k>N/2 not set, left as 0s
-//	for(k=N/2+1;k<N;k++) freq[k] = a*(k-N);                                          // should not be useful
+        for(s=0;s<S;s++) scale[s] = pow(2.,s/4.);                              // set scales with dj=0.25
+	for(k=0;k<=N/2;k++) freq[k] = a*k;                                     // k>N/2 not set, should be left as 0s
+//	for(k=N/2+1;k<N;k++) freq[k] = a*(k-N);                                // should not be useful
 
 
 #if !defined(AVX) || AVX == 0 || SVML == 0
         for(s=0;s<S;s++)
 	for(k=0;k<=N/2;k++)
-            wavefunc[s][k] = exp(-0.5*pow(scale[s]*freq[k]-param,2.))*b;                // k>N/2 not set
+            wavefunc[s][k] = exp(-0.5*pow(scale[s]*freq[k]-param,2.))*b;       // k>N/2 not set, should be left as 0s
 #elif AVX512 == 0
         if constexpr(sizeof(Type) == 8) {
             for(s=0;s<S;s++) {

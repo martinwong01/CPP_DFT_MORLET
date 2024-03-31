b198ea568fa54b449b62fef1005e43c79bc64436
martinwong01 <129178285+martinwong01@users.noreply.github.com>
2024-03-31 10:37:23 +0800

    Update compile.sh

--- v1.1/compile.sh
+++ v1.1/compile.sh
@@ -2,7 +2,7 @@
 
 
 vector=1                       # 1: use avx/avx512/fma/svml, 0: not used
-compiler="clang"               # "clang" or "intel" or "gnu" or "nvidia"
+compiler="clang"               # "clang" or "intel" or "gnu"
 maxn=131072                    # set to 2^
 maxs=64                        # maximum number of wavelet scales
 align=64                       # 512-bit avx, set 64, 256-bit AVX, set 32
@@ -42,9 +42,9 @@ elif [ $compiler == "intel" ]; then
 elif [ $compiler == "clang" ]; then
     #command="clang++-18 $vector_flags -std=c++23 -fopenmp=libomp -Ofast -march=skylake-avx512 $macros"
     command="clang++-18 $vector_flags -std=c++23 -fopenmp=libomp -Ofast $macros"
-elif [ $compiler == "nvidia" ]; then
-    #command="nvc++ $vector_flags -std=c++23 -fopenmp -Ofast -march=skylake-avx512 $macros --diag_suppress declared_but_not_referenced --diag_suppress set_but_not_used"
-    command="nvc++ $vector_flags -std=c++23 -fopenmp -Ofast $macros --diag_suppress declared_but_not_referenced --diag_suppress set_but_not_used"
+#elif [ $compiler == "nvidia" ]; then
+#    #command="nvc++ $vector_flags -std=c++23 -fopenmp -Ofast -march=skylake-avx512 $macros --diag_suppress declared_but_not_referenced --diag_suppress set_but_not_used"
+#    command="nvc++ $vector_flags -std=c++23 -fopenmp -Ofast $macros --diag_suppress declared_but_not_referenced --diag_suppress set_but_not_used"
 fi
 
 
